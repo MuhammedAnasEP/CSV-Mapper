b91f558ffa1b87f5a430aa70358069d6d6ab14ca
MuhammedAnasEP <anasep1110@gmail.com>
2023-12-03 10:55:40 +0530

    created templates folder file in root directory and created home.html file and desgined and connect this html file to backend using Jinja in main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates('templates')
+
+
 @app.get('/')
-def test():
-    return "Hello world!"
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
