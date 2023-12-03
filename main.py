2a8e1533c239bd013d47b9e55c61ae145e163377
MuhammedAnasEP <anasep1110@gmail.com>
2023-12-03 18:51:18 +0530

    created database.py and connected to sqllite and created models.py write code for users tabe and setup models in main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import models
+from database import SessionLocal, engine
 
 app = FastAPI()
 
 templates = Jinja2Templates('templates')
 
+models.Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get('/')
 def home(request: Request):
